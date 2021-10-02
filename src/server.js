c43db4e30acd8b149414ec0d228b0a87bf50b331
Artemy Magarin <magar.art@yandex.ru>
2021-10-02 17:14:08 +0300

    revert port

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8080;
 const fastify = Fastify({ logger: true });
 
 app(fastify)
-  .listen(8080)
+  .listen(PORT)
   .then(() => console.log(`Started server on port ${PORT}`))
   .catch((err) => {
     fastify.log.error(err);
